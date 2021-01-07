33a83d9a1421a9e745e9b9d9265918d7a2512e36
Maitreya775 <76987838+Maitreya775@users.noreply.github.com>
2021-01-07 18:59:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var a,b;
+function setup(){
+  create Canvas(800,400);
+  a=createSprite(400,200,50,12)
+  a.shapeColor="green"
+  b.shapecolor="green"
+  b=createSprite(200,100,30,12)
+}
+
+function draw(){
+  background("black")
+  drawSprites();
+}
